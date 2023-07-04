3f0a8d1a1fe68b69c04738e228fc275ffbde46c0
Natalia <nata.angeli@gmail.com>
2023-07-04 19:48:39 +0300

    zadacha2

---
+++ C1/homework/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Console.Write("Введите первое число:");
+//double num1 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("Введите второе число:");
+//double num2 = Convert.ToDouble(Console.ReadLine());
+
+//if (num1 > num2)
+//{
+//   Console.WriteLine("первое число больше второго.");
+//}
+//else
+//{
+//    Console.WriteLine(" второе число больше первого");
+//}
+
+Console.Write("Введите первое число:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите третье число:");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+double max = num1;
+
+if (num2 > max)
+{
+   max = num2;
+}
+if (num3 > max);
+{
+    max = num3;
+}
+Console.WriteLine("Самое большое число {max}");
